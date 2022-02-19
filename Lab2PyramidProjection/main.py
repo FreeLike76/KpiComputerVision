b4d9ff05f2b010e2fd567d1dec59b8eda6f5ec20
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2022-02-19 13:51:43 +0200

    refactoring and interactive rotations

--- Lab2PyramidProjection/main.py
+++ Lab2PyramidProjection/main.py
@@ -1,63 +1,49 @@
 import numpy as np
 import cv2
-import math
+from pyramid import Pyramid
 
 
 # FUNCTIONS
+def draw_pyramid_lines(img, pyramid, offset_left=0, offset_top=0):
+    # edge color change
+    pyramid.edge_brightness += 8
+    # if almost white
+    if pyramid.edge_brightness >= 255 * 0.8:
+        pyramid.edge_brightness = 0
 
-def draw_pyramid_lines(img, matrix, offset_left=0, offset_top=0):
     # bottom square
-    square = matrix[1:, :]
-
+    square = pyramid.matrix[1:, :]
     # from H to other
     for j in range(0, square.shape[0]):
         cv2.line(img,
-                 (int(matrix[0, 0] + offset_left), int(matrix[0, 1] + offset_top)),
+                 (int(pyramid.matrix[0, 0] + offset_left), int(pyramid.matrix[0, 1] + offset_top)),
                  (int(square[j, 0] + offset_left), int(square[j, 1] + offset_top)),
-                 (0, 0, 0), 1)
+                 (pyramid.edge_brightness, pyramid.edge_brightness, pyramid.edge_brightness), 1)
 
     # bottom square lines
     for j in range(-1, square.shape[0] - 1):
         cv2.line(img,
                  (int(square[j, 0] + offset_left), int(square[j, 1] + offset_top)),
                  (int(square[j + 1, 0] + offset_left), int(square[j + 1, 1] + offset_top)),
-                 (0, 0, 0), 1)
-
-
-def rotate_matrix_x(angle):
-    return np.array([[1, 0, 0, 0],
-                     [0, math.cos(math.radians(angle)), math.sin(math.radians(angle)), 0],
-                     [0, -math.sin(math.radians(angle)), math.cos(math.radians(angle)), 0],
-                     [0, 0, 0, 1]])
-
-
-def rotate_matrix_y(angle):
-    return np.array([[math.cos(math.radians(angle)), 0, -math.sin(math.radians(angle)), 0],
-                     [0, 1, 0, 0],
-                     [math.sin(math.radians(angle)), 0, math.cos(math.radians(angle)), 0],
-                     [0, 0, 0, 1]])
+                 (pyramid.edge_brightness, pyramid.edge_brightness, pyramid.edge_brightness), 1)
 
 
 # CREATE PYRAMID
-pyramid_a = 50
-pyramid_h = 200
-pyramid = np.array([[0, 0, pyramid_h, 0],  # H
-                    [pyramid_a, pyramid_a, 0, 0],  # A
-                    [-pyramid_a, pyramid_a, 0, 0],  # B
-                    [-pyramid_a, -pyramid_a, 0, 0],  # C
-                    [pyramid_a, -pyramid_a, 0, 0]])  # D
+pyramid = Pyramid(50, 100)
 
 # PRINT AND PLOT
 
 while True:
     img = np.ones((480, 640, 3), dtype=np.uint8) * 255
-    print("Pyramid:\n", pyramid)
     draw_pyramid_lines(img, pyramid, offset_left=320, offset_top=240)
     cv2.imshow("Display", img)
     key = cv2.waitKey(0)
+    print(key)
     # X
     if key == 121:
-        pyramid = np.dot(pyramid, rotate_matrix_x(1))
+        pyramid.rotate_x(1)
     # Y
     if key == 120:
-        pyramid = np.dot(pyramid, rotate_matrix_y(1))
+        pyramid.rotate_y(1)
+    if key == 122:
+        pyramid.rotate_z(1)
