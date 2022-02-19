6b412279f9f80ba5e4f6a932a1b098a017ab29c6
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2022-02-19 13:12:32 +0200

    rotations

--- Lab2PyramidProjection/main.py
+++ Lab2PyramidProjection/main.py
@@ -24,27 +24,40 @@ def draw_pyramid_lines(img, matrix, offset_left=0, offset_top=0):
                  (0, 0, 0), 1)
 
 
-def rotate_matrix(angle): # OLD
-    return np.array([[math.cos(math.radians(angle)), -math.sin(math.radians(angle)), 0],
-                     [math.sin(math.radians(angle)), math.cos(math.radians(angle)), 0],
-                     [0, 0, 1]])
+def rotate_matrix_x(angle):
+    return np.array([[1, 0, 0, 0],
+                     [0, math.cos(math.radians(angle)), math.sin(math.radians(angle)), 0],
+                     [0, -math.sin(math.radians(angle)), math.cos(math.radians(angle)), 0],
+                     [0, 0, 0, 1]])
 
 
-# CREATE PENTAGON MATRIX
+def rotate_matrix_y(angle):
+    return np.array([[math.cos(math.radians(angle)), 0, -math.sin(math.radians(angle)), 0],
+                     [0, 1, 0, 0],
+                     [math.sin(math.radians(angle)), 0, math.cos(math.radians(angle)), 0],
+                     [0, 0, 0, 1]])
+
+
+# CREATE PYRAMID
 pyramid_a = 50
 pyramid_h = 200
-pyramid = np.array([[0, 0, pyramid_h, 0],               # H
-                    [pyramid_a, pyramid_a, 0, 0],       # A
-                    [-pyramid_a, pyramid_a, 0, 0],      # B
-                    [-pyramid_a, -pyramid_a, 0, 0],     # C
-                    [pyramid_a, -pyramid_a, 0, 0]])     # D
-
+pyramid = np.array([[0, 0, pyramid_h, 0],  # H
+                    [pyramid_a, pyramid_a, 0, 0],  # A
+                    [-pyramid_a, pyramid_a, 0, 0],  # B
+                    [-pyramid_a, -pyramid_a, 0, 0],  # C
+                    [pyramid_a, -pyramid_a, 0, 0]])  # D
 
 # PRINT AND PLOT
-temp = pyramid.copy()
-img = np.ones((640, 480, 3), dtype=np.uint8) * 255
-for i in range(5):
-    print("Pyramid:\n", temp)
-    draw_pyramid_lines(img, temp, offset_left=160, offset_top=120)
+
+while True:
+    img = np.ones((480, 640, 3), dtype=np.uint8) * 255
+    print("Pyramid:\n", pyramid)
+    draw_pyramid_lines(img, pyramid, offset_left=320, offset_top=240)
     cv2.imshow("Display", img)
-    cv2.waitKey(500)
+    key = cv2.waitKey(0)
+    # X
+    if key == 121:
+        pyramid = np.dot(pyramid, rotate_matrix_x(1))
+    # Y
+    if key == 120:
+        pyramid = np.dot(pyramid, rotate_matrix_y(1))
